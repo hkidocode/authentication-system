2826c6ac7765981e99b752884bdd6cf655667e1e
hkidocode <57941226+hkidocode@users.noreply.github.com>
2021-07-06 13:59:57 +0100

    add files project and setup application.properties

---
+++ src/test/java/ma/pragmatic/authenticationsystem/AuthenticationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.pragmatic.authenticationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
